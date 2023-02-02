5206bb2aa1c62aca428bb551a881b853bc993cff
TrungLeo[HPI-Tech] <trung.pham@hoang-phuc.net>
2023-02-02 22:42:24 +0700

    :sparkles: feat(setup): init

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+import "ant-design-vue/dist/antd.css";
+import "uno.css";
+
+createApp(App).mount("#app");
---
+++ unocss.config.ts
@@ -0,0 +1,30 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno(), // Mặc định (@unocss/preset-wind).
+    presetAttributify(), // CSS trên DOM như 1 Attribute(@unocss/preset-attributify)
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }), // Sử dụng bất kỳ icon nào làm class or attribute.(@unocss/preset-icons)
+    presetWebFonts({
+      fonts: {
+        sans: "Roboto",
+        mono: ["Fira Code", "Fira Mono:400,700"],
+      },
+    }), // sử dụng bất kì Font mà thư viện có sẵn hoặc có thể tự custome font (@unocss/preset-web-fonts)
+  ],
+  transformers: [
+    transformerDirectives(), // @apply, @screen and theme() directive (@unocss/transformer-directives)
+    transformerVariantGroup(), // nhóm CSS lại với nhau => hover:(bg-gray-400 font-medium) (@unocss/transformer-variant-group)
+  ],
+});
